551d87ddc1a66b9776dbfb3a94cfe8ce2d624246
harrry1412 <88812209+harrry1412@users.noreply.github.com>
2023-12-15 18:11:50 -0500

    搜索排除记录列
    优化版本号修改更新

--- AboutDialog.py
+++ AboutDialog.py
@@ -5,9 +5,10 @@ from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import Qt
 
 class AboutDialog(QDialog):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, version="1.0.0"):
         super(AboutDialog, self).__init__(parent)
-        self.initUI()
+        self.version = version  # 首先设置 version 属性
+        self.initUI()           # 然后调用 initUI
 
     def initUI(self):
         self.setWindowTitle("关于")
@@ -15,8 +16,8 @@ class AboutDialog(QDialog):
         self.resize(800, 600)
 
         about_text = (
-            "Viva Stock Management System / Viva大仓库库存管理系统\n"
-            "版本: 2.6.4\n"
+            f"Viva Stock Management System / Viva大仓库库存管理系统\n"
+            f"版本: {self.version}\n"  # 现在这里可以正确访问 version 属性
             "作者: Harry\n"
             "版权: © 2023 Haochu Chen\n"
             "描述: Viva库存管理系统，旨在管理并规范化所有增减库存的活动，使得记录清晰易读可追踪。"
